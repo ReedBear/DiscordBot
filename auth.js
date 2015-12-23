eacb2b851d697bc401799b8ae140e92e021c8723
Arthur Jamain <arthur.jamain@gmail.com>
2015-12-23 15:37:57 +0100

    adds imgur

--- auth.js
+++ auth.js
@@ -5,5 +5,6 @@ module.exports = {
     "google_custom_search": process.env.GOOGLE_CUSTOM_SEARCH,
     "imgflip_username": process.env.IMGFLIP_USERNAME,
     "imgflip_password": process.env.IMGFLIP_PASSWORD,
-    "wolfram_api_key": process.env.WOLFRAM_API
+    "wolfram_api_key": process.env.WOLFRAM_API,
+    "imgur_client_id": process.env.IMGUR_CLIENT_ID
 }
